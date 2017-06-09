07d975f00f7fc71b90156ab8d53ac0daf18710d0
rpghero <rpghero78@hotmail.com>
2017-06-09 17:30:57 +0900

    Create cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,24 @@
 makeCacheMatrix <- function(x = matrix()) {
-  invrs <- NULL
-  set <- function(y) {
-    x <<- y
-    invrs <<- NULL
-  }
-  get <- function() x
-  set_inverse <- function(inverse) invrs <<- inverse
-  get_inverse <- function() invrs
-  list(set = set, get = get,
-       set_inverse = set_inverse,
-       get_inverse = get_inverse)
+  original_matrix <- x
+  inverted_matrix <- NULL
+## this create list, which will hold original matrix
+## and inverted matrix as matrix which makes it
+## easier for check
+  list(original_matrix = original_matrix,
+       inverted_matrix = inverted_matrix)
 }
 
-cacheSolve <- function(x, ...) {
-  invrs <- x$get_inverse()
-  if(!is.null(invrs)) {
+cacheSolve <- function(x) {
+## if x$inverted_matrix hold cached inverted matrix
+## then it will simply return inverted matrix
+  if(!is.null(x$inverted_matrix)) {
     message("getting cached data")
-    return(invrs)
+    return(x$inverted_matrix)
   }
-  mat <- x$get()
-  invrs <- solve(mat, ...)
-  x$set_inverse(invrs)
-  invrs
+## if x$inverted_matrix is NULL, it will calculating
+## inverted_matrix, and cache it to list of x.
+  mat <- x$original_matrix
+  solved_mat <- solve(mat)
+  x$inverted_matrix <<- solved_mat
+  solved_mat
 }
